647cd7fd7f8d0a8ecaf0d84d6ca33865b9718efc
Zachary-Kiz <zachary.kizell@gmail.com>
2025-10-06 14:10:02 -0400

    add restart after game over

--- Assets/Scripts/GameUiHandler.cs
+++ Assets/Scripts/GameUiHandler.cs
@@ -1,16 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DisplayHealth : MonoBehaviour
 {
     public TextMeshProUGUI dispHealth;
     public TextMeshProUGUI dispGameOver;
+    public GameObject restartButton;
     private PlayerObject playerObject;
     // Start is called before the first frame update
     void Start()
     {
+        restartButton.SetActive(false);
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         if (player != null)
         {
@@ -24,10 +25,16 @@ public class DisplayHealth : MonoBehaviour
         if (playerObject.health < 1)
         {
             dispGameOver.enabled = true;
+            restartButton.SetActive(true);
         }
         if (playerObject != null)
         {
             dispHealth.text = $"Health: {playerObject.health}";
         }
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
